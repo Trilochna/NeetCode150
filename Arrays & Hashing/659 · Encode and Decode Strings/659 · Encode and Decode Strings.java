9385b1c1411421b16cc0f6a56ad583e3c70299e1
TRILOCHNA <vaidtrilochna@gmail.com>
2023-07-29 14:30:48 +0530

    Update 659 · Encode and Decode Strings.java

--- Arrays & Hashing/659 · Encode and Decode Strings/659 · Encode and Decode Strings.java
+++ Arrays & Hashing/659 · Encode and Decode Strings/659 · Encode and Decode Strings.java
@@ -1,1 +1,39 @@
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        // write your code here
 
+        StringBuilder encodedString = new StringBuilder();
+
+        for(String str : strs){
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        // write your code here
+
+        List<String> list = new ArrayList<>();
+
+        int i = 0;
+        while(i < str.length()){
+            int j = i;
+            while(str.charAt(j) != '#'){
+                j++;
+            }
+
+            int length = Integer.valueOf(str.substring(i, j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1, i));
+        }
+        return list;
+    }
+}
