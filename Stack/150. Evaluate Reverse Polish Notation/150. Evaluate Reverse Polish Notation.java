63d884c818fb61d2aa4dc882488a66ff28e50ee8
TRILOCHNA <vaidtrilochna@gmail.com>
2023-07-30 12:27:15 +0530

    Update 150. Evaluate Reverse Polish Notation.java

--- Stack/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.java
+++ Stack/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.java
@@ -1,1 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        
+        Stack<Integer> stack = new Stack();;
 
+        for(String c : tokens){
+            if(c.equals("+")){
+                stack.add(stack.pop() + stack.pop());
+            } else if(c.equals("-")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b - a);
+            } else if(c.equals("*")){
+                stack.add(stack.pop() * stack.pop());
+            } else if(c.equals("/")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b / a);
+            } else{
+                stack.add(Integer.parseInt(c));
+            }
+        }
+
+        return stack.pop(); 
+    }
+}
+
+// time and memory complexity is O(n) because we are only parsing through stack 
