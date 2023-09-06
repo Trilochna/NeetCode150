ea54dd8f46a045a64ac5ead20159d368946f6a98
TRILOCHNA <vaidtrilochna@gmail.com>
2023-09-06 18:04:06 +0530

    Create Demo.java

---
+++ Backtracking/Demo/Demo.java
@@ -0,0 +1,24 @@
+public class Main
+{
+    
+    public static void printPermutation(String str, String perm, int index){
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i = 0; i < str.length(); i++){
+            char currCharacter = str.charAt(i);
+            // because already used character channot be used again
+            // compute other characters
+            String newString = str.substring(0, i) + str.substring(i + 1);
+            // now ass we have solved one problem 
+            // use recursion
+            printPermutation(newString, perm + currCharacter, index + 1);
+        }    
+    }
+    
+	public static void main(String[] args) {
+		String str = "ABC";
+		printPermutation(str, "", 0);
+	}
+}
