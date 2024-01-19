4a114c57c12ffef926746ed1d0788f2bd4a850a0
TRILOCHNA <vaidtrilochna@gmail.com>
2024-01-19 14:50:40 +0530

    Update 217. Contains Duplicate.java

--- Arrays & Hashing/217. Contains Duplicate/217. Contains Duplicate.java
+++ Arrays & Hashing/217. Contains Duplicate/217. Contains Duplicate.java
@@ -1,1 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> map1 = new HashSet<>();
 
+        for(int i = 0; i < nums.length; i++){
+            if(map1.contains(nums[i])){
+                return true;
+            }
+            map1.add(nums[i]);
+        }
+        return false;
+    }
+}
